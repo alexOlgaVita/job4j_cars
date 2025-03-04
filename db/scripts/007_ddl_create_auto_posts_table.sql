b11243bd33671a322013c988743aee483005979b
olga.ilyina <olga.ilyina@axenix.pro>
2025-03-04 14:18:24 +0300

    1. Фильтры для объявлений машин [#4745]

--- db/scripts/007_ddl_create_auto_posts_table.sql
+++ db/scripts/007_ddl_create_auto_posts_table.sql
@@ -1,6 +1,6 @@
 create table auto_posts
 (
-    id   serial primary key,
+    id serial primary key,
     description varchar not null,
     created timestamp not null,
     auto_user_id int  references auto_users(id),
---
+++ src/main/java/ru/job4j/repository/PostRepository.java
@@ -0,0 +1,100 @@
+package ru.job4j.repository;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+import ru.job4j.model.Post;
+
+import javax.persistence.Entity;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Entity
+/* @Table(name = "posts") */
+@Repository
+@AllArgsConstructor
+public class PostRepository {
+
+    private final CrudRepository crudRepository;
+
+    /**
+     * Сохранить в базе.
+     *
+     * @param post объявление.
+     * @return объявление с id.
+     */
+    public Post create(Post post) {
+        crudRepository.run(session -> session.persist(post));
+        return post;
+    }
+
+    /**
+     * Обновить в базе объявления.
+     *
+     * @param post объявление.
+     */
+    public void update(Post post) {
+        crudRepository.run(session -> session.merge(post));
+    }
+
+    /**
+     * Удалить объявления по id.
+     *
+     * @param postId ID
+     */
+    public void delete(int postId) {
+        crudRepository.run(
+                "delete from Post where id = :fId",
+                Map.of("fId", postId)
+        );
+    }
+
+    /**
+     * Список объявление отсортированных по id.
+     *
+     * @return список объявлений.
+     */
+    public List<Post> findAllOrderById() {
+        return crudRepository.query("from Post order by id asc", Post.class);
+    }
+
+    /**
+     * Найти объявления по ID
+     *
+     * @return объявление.
+     */
+    public Optional<Post> findById(int postId) {
+        return crudRepository.optional(
+                "from Post where id = :fId", Post.class,
+                Map.of("fId", postId)
+        );
+    }
+
+    /**
+     * Найти объявления за последний день
+     *
+     * @return объявление.
+     */
+    public List<Post> findForLastDay() {
+        return crudRepository.query("from Post p1 where p1.created in (select max(p2.created) from Post p2", Post.class);
+    }
+
+    /**
+     * Найти объявления с фото
+     *
+     * @return объявление.
+     */
+    public List<Post> findWithPhoto() {
+        return null;
+    }
+
+    /**
+     * Найти объявления определенной марки
+     *
+     * @param brand марка
+     * @return объявление.
+     */
+    public List<Post> findByBrand(String brand) {
+        return null;
+    }
+}
